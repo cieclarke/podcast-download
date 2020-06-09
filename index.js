fd0f6a764e4bdf774992ecc17f9f3f97c8a6a332
cieclarke <chris@cieclarke.com>
2020-06-09 08:46:48 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const PodcastFiles = require('./lib/PodcastFiles');
+const Feed = require('./lib/PodcastFeeds');
+const Episodes = require('./lib/PodcastEpisodes');
+const Files = require('./lib/Files');
+const podcastDir = process.env.PODCAST_DIR;
+const feedFilePath = process.env.PODCAST_FEEDS;
+
+const f = new Feed(feedFilePath);
+const files = new Files(podcastDir);
+const currentFiles = files.All();
+
+f.on('data', (d) => {
+    const e = new Episodes(d);
+    e.on('data', (podcast) => {
+        currentFiles.then((files) => {
+            const extantfile = files.find((file, index, arr) => {
+                return file.data.artist == podcast.artist &&
+                file.data.album == podcast.album &&
+                file.data.guid == podcast.guid; 
+            });
+            
+            if(extantfile === undefined) {
+                const podcastFiles = new PodcastFiles(podcast, podcastDir);
+                podcastFiles.Write();
+            };
+        });
+        
+    });
+    e.Start();
+});
+
+f.Start();
