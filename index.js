d0f07048392b2fb65d7f3ce500d51b68569eb24a
cieclarke <chris@cieclarke.com>
2020-06-18 10:41:20 +0100

    promise queue added to stop overload

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const PodcastFiles = require('./lib/PodcastFiles');
 const Feed = require('./lib/PodcastFeeds');
 const Episodes = require('./lib/PodcastEpisodes');
 const Files = require('./lib/Files');
+const Queue = require('promise-queue');
 const podcastDir = process.env.PODCAST_DIR;
 const feedFilePath = process.env.PODCAST_FEEDS;
-
+const queue = new Queue(1, Infinity);
 const f = new Feed(feedFilePath);
 const files = new Files(podcastDir);
 const currentFiles = files.All();
-
-console.log(currentFiles);
 
 f.on('data', (d) => {
     const e = new Episodes(d);
@@ -23,8 +22,10 @@ f.on('data', (d) => {
             
             if(extantfile === undefined) {
                 const podcastFiles = new PodcastFiles(podcast, podcastDir);
-                podcastFiles.Write();
-            };
+                queue.add(() => { return podcastFiles.Write(); });
+            } else {
+                console.log('exists: ' + extantfile.file);
+            }
         });
         
     });
